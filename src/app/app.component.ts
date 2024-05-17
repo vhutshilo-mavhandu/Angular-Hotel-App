39c9f23c0e7d7b95b160f983620736994171574c
Vhutshilo V. Mavhandu <VhutshiloCohort11@digilink.africa>
2024-05-17 13:39:20 +0200

    first-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
+import {RouterLink,RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
